796dcbb963329cc29f1d18f7db0953defe78b448
Shuga69 <54189758+Shuga69@users.noreply.github.com>
2021-07-16 15:17:28 +0300

    chnage button

--- frontend/src/Components/Groups/GroupSignUp.js
+++ frontend/src/Components/Groups/GroupSignUp.js
@@ -40,8 +40,8 @@ const GroupMoreInfo = ({ handleClose, id, show, children }) => {
                     <input className="custom-input" type="text" placeholder="Ім'я" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                     <input className="custom-input" type="text" placeholder="Прізвище" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
                     <div className="button-box">
-                        <Button type="submit" text="Записатись"/>
-                    <Button onClick={handleClose} text="Закрити"/>
+                        <button type="submit">Записатись</button>
+                    {/*<Button onClick={handleClose} text="Закрити"/>*/}
                     </div>
                     </div>
                 </form>
